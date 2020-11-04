c19b54b2d467303e2da54b1f186e86ac86c0a814
megane42 <h.kzm.gm@gmail.com>
2020-11-04 23:32:54 +0900

    rule css mocule naming & placement

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Footer from '../components/footer'
-import styles from '../styles/Home.module.css'
+import styles from './index.module.css'
 
 export default function Home() {
   return (
