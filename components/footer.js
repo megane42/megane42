4bbcc31d1a4c982f572e22be8ec39c70c9a61768
megane42 <h.kzm.gm@gmail.com>
2020-11-11 23:54:41 +0900

    footer component のスタイルを tailwind だけで表現するようにした

--- components/footer.js
+++ components/footer.js
@@ -1,15 +1,14 @@
-import styles from './footer.module.css'
-
 export default function Footer() {
   return (
-    <footer className={styles.footer}>
+    <footer className="w-full h-24 border-t border-solid border-gray-300 flex justify-center items-center">
       <a
         href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
         rel="noopener noreferrer"
+        target="_blank"
+        className="flex justify-center items-center"
       >
         Powered by{' '}
-        <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        <img src="/vercel.svg" alt="Vercel Logo" className="h-4 mx-1" />
       </a>
     </footer>
   )
