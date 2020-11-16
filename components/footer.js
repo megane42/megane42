dfc60daf6a1cdff9b69d4718d39ce70858b58351
megane42 <h.kzm.gm@gmail.com>
2020-11-16 21:52:19 +0900

    tailwind が html を汚すのが好きになれないので、やっぱり入れないことにした
    This reverts commit 4bbcc31d1a4c982f572e22be8ec39c70c9a61768.
    This reverts commit 4794761cb50452ea45db5dcd9d8b394c3aed3ad9.

--- components/footer.js
+++ components/footer.js
@@ -1,14 +1,15 @@
+import styles from './footer.module.css'
+
 export default function Footer() {
   return (
-    <footer className="w-full h-24 border-t border-solid border-gray-300 flex justify-center items-center">
+    <footer className={styles.footer}>
       <a
         href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        rel="noopener noreferrer"
         target="_blank"
-        className="flex justify-center items-center"
+        rel="noopener noreferrer"
       >
         Powered by{' '}
-        <img src="/vercel.svg" alt="Vercel Logo" className="h-4 mx-1" />
+        <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
       </a>
     </footer>
   )
