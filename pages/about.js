1065e3214d102a92004bc6cc081f4c04f3b3aca4
megane42 <h.kzm.gm@gmail.com>
2021-02-09 00:34:32 +0900

    about ページの中身をちょっと埋めた

--- pages/about.js
+++ pages/about.js
@@ -17,8 +17,11 @@ export default function About() {
         <h1>about</h1>
         <ul className={styles.profileList}>
           <li>@megane42</li>
-          <li>Tokyo</li>
-          <li>好きなパン: クロワッサン</li>
+          <li>Tokyo, Japan</li>
+          <li><a href="https://twitter.com/megane42" target="_blank" rel="noopener">twitter</a></li>
+          <li><a href="https://github.com/megane42" target="_blank" rel="noopener">github</a></li>
+          <li><a href="https://qiita.com/megane42" target="_blank" rel="noopener">qiita</a></li>
+          <li><a href="https://speakerdeck.com/megane42/" target="_blank" rel="noopener">speakerdeck</a></li>
         </ul>
       </main>
 
