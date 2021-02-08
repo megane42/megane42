fdff1fa5e8135f96fc445c3dd307a3d3e0a48066
megane42 <h.kzm.gm@gmail.com>
2021-02-09 01:43:25 +0900

    about の中身をさらに追加

--- pages/about.js
+++ pages/about.js
@@ -14,15 +14,40 @@ export default function About() {
       <Header />
 
       <main className={styles.main}>
-        <h1>about</h1>
-        <ul className={styles.profileList}>
-          <li>@megane42</li>
-          <li>Tokyo, Japan</li>
-          <li><a href="https://twitter.com/megane42" target="_blank" rel="noopener">twitter</a></li>
-          <li><a href="https://github.com/megane42" target="_blank" rel="noopener">github</a></li>
-          <li><a href="https://qiita.com/megane42" target="_blank" rel="noopener">qiita</a></li>
-          <li><a href="https://speakerdeck.com/megane42/" target="_blank" rel="noopener">speakerdeck</a></li>
-        </ul>
+        <div className={styles.row}>
+
+          <div className={styles.block}>
+            <h1>about</h1>
+            <ul className={styles.list}>
+              <li>@megane42</li>
+              <li>Tokyo, Japan</li>
+              <li><a href="https://twitter.com/megane42" target="_blank" rel="noopener">twitter</a></li>
+              <li><a href="https://github.com/megane42" target="_blank" rel="noopener">github</a></li>
+              <li><a href="https://qiita.com/megane42" target="_blank" rel="noopener">qiita</a></li>
+              <li><a href="https://speakerdeck.com/megane42/" target="_blank" rel="noopener">speakerdeck</a></li>
+            </ul>
+          </div>
+
+          <div className={styles.block}>
+            <h1>product</h1>
+            <ul className={styles.list}>
+              <li><a href="https://starlight.tsudanuma-studio.net/" target="_blank" rel="noopener">starlight loading</a></li>
+              <li><a href="https://dj-giftee.web.app/" target="_blank" rel="noopener">dj-giftee</a>
+                <ul className={styles.list}>
+                  <li><a href="https://speakerdeck.com/megane42/how-to-make-dj-giftee" target="_blank" rel="noopener">description</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+
+          <div className={styles.block}>
+            <h1>weight</h1>
+            <ul className={styles.list}>
+              <li>TODO</li>
+            </ul>
+          </div>
+
+        </div>
       </main>
 
       <Footer />
